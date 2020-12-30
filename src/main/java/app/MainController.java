1250237b22a76252b743ebec389e8862de86eae4
Aring8510 <nakamu8510@gmail.com>
2020-12-30 18:35:16 +0900

    can input by sourceInput

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -2,64 +2,24 @@ package app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.io.IOException;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import app.storage.StorageService;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
-    /*
-    private final StorageService storageService;
-
-    @Autowired
-    public MainController(StorageService storageService) {
-        this.storageService = storageService;
-    }
-    @GetMapping("/upload")
-    public String listUploadedFiles(Model model) throws IOException {
-        model.addAttribute("files", storageService.loadAll().map(path -> MvcUriComponentsBuilder.fromMethodName(MainController.class, "serveFile", path.getFileName().toString()).build().toUri().toString()).collect(Collectors.toList()));
-        return "uploadForm";
-    }
-    @GetMapping("/file/{filename:.+}")
-    @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-        Resource file = storageService.loadAsResource(filename);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename() + "\"").body(file);
-
-    }
-    @PostMapping("/")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
-
-        storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
-        return "redirect:/";
-    }
-
-
-*/
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String home(Model model) {
         model.addAttribute("msg", "Hello World!!");
         return "index";
+    }
+    @GetMapping(value = "/sourceInput")
+    public String sourceInputForm (Model model) {
+        model.addAttribute("sourceInput", new SourceInput());
+        return "sourceInput";
+    }
+    @PostMapping(value = "/sourceInput")
+    public String sourceInputSubmit (@ModelAttribute SourceInput si,  Model model) {
+        System.out.println("submit");
+        model.addAttribute("sourceInput", si);
+        return "result";
     }
 }
---
+++ src/main/java/app/SourceInput.java
@@ -0,0 +1,15 @@
+package app;
+
+public class SourceInput{
+
+    private String source;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+}
